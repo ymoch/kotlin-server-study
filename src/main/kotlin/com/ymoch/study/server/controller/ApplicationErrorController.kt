9e91a8c13c43c1a0a62841f0c6fc4d584225b1bf
ymoch <ymoch.dev@gmail.com>
2018-09-29 22:34:46 +0900

    Refactor.

--- src/main/kotlin/com/ymoch/study/server/controller/ApplicationErrorController.kt
+++ src/main/kotlin/com/ymoch/study/server/controller/ApplicationErrorController.kt
@@ -3,21 +3,21 @@ package com.ymoch.study.server.controller
 import com.ymoch.study.server.entity.ErrorRecord
 import com.ymoch.study.server.exception.ApplicationRuntimeException
 import com.ymoch.study.server.service.ErrorService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.context.request.WebRequest
 import javax.servlet.http.HttpServletResponse
 
+@Component
 @RestController
 @RequestMapping(path = ["error"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
-class ApplicationErrorController : ErrorController {
-
-    @Autowired
-    private lateinit var errorService: ErrorService
+class ApplicationErrorController(
+        private val errorService: ErrorService
+) : ErrorController {
 
     override fun getErrorPath(): String = "/error"
 
--- src/main/kotlin/com/ymoch/study/server/service/impl/ErrorServiceImpl.kt
+++ src/main/kotlin/com/ymoch/study/server/service/impl/ErrorServiceImpl.kt
@@ -3,22 +3,19 @@ package com.ymoch.study.server.service.impl
 import com.ymoch.study.server.entity.ErrorRecord
 import com.ymoch.study.server.exception.ApplicationRuntimeException
 import com.ymoch.study.server.service.ErrorService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.servlet.error.ErrorAttributes
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 import org.springframework.web.context.request.WebRequest
 
 val DEFAULT_STATUS = HttpStatus.INTERNAL_SERVER_ERROR
 
 @Component
-@Service
-class ErrorServiceImpl(private val errorAttributes: ErrorAttributes) : ErrorService {
-
-    @Value("\${debug:false}")
-    private var debug = false
+class ErrorServiceImpl(
+        private val errorAttributes: ErrorAttributes,
+        @Value("\${debug:false}") private val debug: Boolean
+) : ErrorService {
 
     override fun createEntity(request: WebRequest): ErrorRecord {
         val error: Throwable? = errorAttributes.getError(request)
