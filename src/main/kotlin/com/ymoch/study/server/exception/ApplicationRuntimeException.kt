f1cedd66175f436781589e38ee10ef13681fb848
ymoch <ymoch.dev@gmail.com>
2018-10-07 02:22:07 +0900

    Add tests.

--- src/main/kotlin/com/ymoch/study/server/exception/ApplicationRuntimeException.kt
+++ src/main/kotlin/com/ymoch/study/server/exception/ApplicationRuntimeException.kt
@@ -1,7 +1,10 @@
 package com.ymoch.study.server.exception
 
 class ApplicationRuntimeException(
-        message: String? = null,
+        message: String?,
         cause: Throwable? = null,
         val status: Int = 500
-) : RuntimeException(message, cause)
+) : RuntimeException(message, cause) {
+    // Make the default constructor secondary to improve then test coverage.
+    constructor() : this(null)
+}
