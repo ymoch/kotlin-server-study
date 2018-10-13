1217e9338af2a200b3818dcff046c6949abcf523
ymoch <ymoch.dev@gmail.com>
2018-10-13 17:20:53 +0900

    Enable Hystrix.

--- src/main/kotlin/com/ymoch/study/server/Application.kt
+++ src/main/kotlin/com/ymoch/study/server/Application.kt
@@ -2,8 +2,12 @@ package com.ymoch.study.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 class Application
 
 fun main(args: Array<String>) {
